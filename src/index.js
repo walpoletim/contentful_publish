1891df5d6dca06ddf6701a11722eb5bb01273c0e
Tim Walpole <walpoletim@googlemail.com>
2021-02-05 08:53:42 +0000

    Updated

--- src/index.js
+++ src/index.js
@@ -1,77 +1,121 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
-import { Button } from '@contentful/forma-36-react-components';
-import { init, locations } from 'contentful-ui-extensions-sdk';
-import tokens from '@contentful/forma-36-tokens';
-import '@contentful/forma-36-react-components/dist/styles.css';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button, Paragraph } from "@contentful/forma-36-react-components";
+import { init } from "contentful-ui-extensions-sdk";
+import "@contentful/forma-36-react-components/dist/styles.css";
+import "./index.css";
 
-export class DialogExtension extends React.Component {
-  static propTypes = {
-    sdk: PropTypes.object.isRequired
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount = () => {
+    this.detachFn = this.props.sdk.entry.onSysChanged(this.onSysChanged);
+
+    this.props.sdk.window.startAutoResizer();
   };
 
-  render() {
+  componentWillUnmount = () => {
+    this.detachFn();
+    if (this.debounceInterval) {
+      clearInterval(this.debounceInterval);
+    }
+  };
+
+  onError = error => {
+    this.setState({ working: false });
+    this.props.sdk.notifier.error(error.message);
+  };
+
+  onSysChanged = () => {
+    if (this.debounceInterval) {
+      clearInterval(this.debounceInterval);
+    }
+    this.debounceInterval = setInterval(this.refreshGatsbyPreview, 1000);
+  };
+
+  refreshGatsbyPreview = () => {
+    if (this.debounceInterval) {
+      clearInterval(this.debounceInterval);
+    }
+
+    const { webhookUrl } = this.props.sdk.parameters.installation;
+
+    fetch(webhookUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-preview-update-source": "contentful-sidebar-extension"
+      },
+      body: JSON.stringify({})
+    }).then(
+      () => this.props.sdk.notifier.success("Gatsby Preview updated!"),
+      () => this.props.sdk.notifier.error("Gatsby Preview failed :(")
+    );
+  };
+
+  openPreviewTab = async () => {
+    const {
+      parameters: { installation, instance },
+      entry
+    } = this.props.sdk;
+    const { previewUrl } = installation;
+    const { contentTypeSlug } = instance;
+    const { slug: contentSlug } = entry.fields;
+
+    try {
+      const slug = await fetch(`${previewUrl}/___graphql`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          query:
+            "query getQualifiedSlug($slugExpr:String) { sitePage( path:{ regex:$slugExpr } ) { path } }",
+          variables: {
+            slugExpr: `/${
+              contentSlug ? contentSlug.getValue() : contentTypeSlug
+            }\/?$/`
+          }
+        })
+      })
+        .then(res => res.json())
+        .then(json => (json && json.data ? json.data.sitePage.path : ``));
+
+      const normalize = part => part.replace(/\/$/, "");
+      window.open(`${normalize(previewUrl)}/${slug}`);
+    } catch (e) {
+      console.error(e);
+
+      let slug = contentTypeSlug ? contentTypeSlug : "";
+
+      if (this.props.sdk.entry.fields.slug) {
+        slug += "/" + contentSlug.getValue();
+      }
+
+      window.open(`${previewUrl}${slug}`);
+    }
+  };
+
+  render = () => {
     return (
-      <div style={{ margin: tokens.spacingM }}>
-        <Button
-          testId="close-dialog"
-          buttonType="muted"
-          onClick={() => {
-            this.props.sdk.close('data from modal dialog');
-          }}>
-          Close modal
-        </Button>
+      <div className="extension">
+        <div className="flexcontainer">
+          <Button
+            buttonType="positive"
+            onClick={this.openPreviewTab}
+            isFullWidth
+          >
+            Build Site
+          </Button>
+
+        </div>
       </div>
     );
-  }
+  };
 }
 
-export class SidebarExtension extends React.Component {
-  static propTypes = {
-    sdk: PropTypes.object.isRequired
-  };
-
-  componentDidMount() {
-    this.props.sdk.window.startAutoResizer();
-  }
-
-  onButtonClick = async () => {
-    const result = await this.props.sdk.dialogs.openExtension({
-      width: 800,
-      title: 'The same extension rendered in modal window'
-    });
-    console.log(result);
-  };
-
-  render() {
-    return (
-      <Button
-        buttonType="positive"
-        isFullWidth={true}
-        testId="open-dialog"
-        onClick={this.onButtonClick}>
-        Click on me to open dialog extension
-      </Button>
-    );
-  }
-}
-
-export const initialize = sdk => {
-  if (sdk.location.is(locations.LOCATION_DIALOG)) {
-    ReactDOM.render(<DialogExtension sdk={sdk} />, document.getElementById('root'));
-  } else {
-    ReactDOM.render(<SidebarExtension sdk={sdk} />, document.getElementById('root'));
-  }
-};
-
-init(initialize);
-
-/**
- * By default, iframe of the extension is fully reloaded on every save of a source file.
- * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
- */
-// if (module.hot) {
-//   module.hot.accept();
-// }
+init(sdk => {
+  ReactDOM.render(<App sdk={sdk} />, document.getElementById("root"));
+});
